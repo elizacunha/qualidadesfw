e37107d6288cba31d3784c79e19b63d92e0011bf
elizacunha <elizacunha@gmail.com>
2017-10-10 19:27:30 -0300

    Primeiro commit contendo estas informacoes de AULA1

---
+++ AULA 1/AULA 1/EFContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace entityFramework
+{
+    class EFContext : DbContext
+    {
+        public EFContext(): base("EF")
+        {
+            DropCreateDatabaseIfModelChanges<EFContext> initializer = new DropCreateDatabaseIfModelChanges<EFContext>();
+            Database.SetInitializer<EFContext>(initializer);           
+        }
+
+        public DbSet<Editora> Editoras { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
